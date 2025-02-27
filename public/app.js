94b03572aaf1a5ab5b14abecb06380afe5bdbaaa
caro270903 <carolina.vargasv26@gmail.com>
2025-02-27 16:55:12 -0500

    version 2

--- public/app.js
+++ public/app.js
@@ -9,3 +9,30 @@ document.querySelectorAll(".go-to-seccion-contacto").forEach(button =>
             document.getElementById("seccion-contacto").scrollIntoView({ behavior: "smooth" });
         });
     });
+
+
+// Selecciona el botón de modo oscuro
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+// Verifica si el modo oscuro está activo en el localStorage
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+// Aplica el modo oscuro si está activo
+if (isDarkMode) {
+    document.body.classList.add('dark-mode');
+    darkModeToggle.textContent = '☀️'; // Cambia el ícono a sol
+}
+
+// Alterna el modo oscuro al hacer clic en el botón
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Cambia el ícono del botón
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = '☀️';
+        localStorage.setItem('darkMode', 'enabled'); // Guarda la preferencia
+    } else {
+        darkModeToggle.textContent = '🌙';
+        localStorage.setItem('darkMode', 'disabled'); // Guarda la preferencia
+    }
+});
